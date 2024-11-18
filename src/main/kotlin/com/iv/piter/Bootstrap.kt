31044ac6fa1e036de69666326a75b38cfe6eed31
iv <ivkuznet1952@gmail.com>
2024-11-18 10:41:38 +0300

    Clean db.

--- src/main/kotlin/com/iv/piter/Bootstrap.kt
+++ src/main/kotlin/com/iv/piter/Bootstrap.kt
@@ -62,18 +62,18 @@ class Bootstrap: ServletContextListener {
         // Initializes the VoK framework
         // Makes sure the database is up-to-date. See src/main/resources/db/migration for db init scripts.
         log.info("Running DB migrations")
-//         val flyway = Flyway.configure().cleanDisabled(false)
-//            .dataSource(JdbiOrm.getDataSource())
-//            .load()
-//        flyway.clean()
-//        sleep(5000)
-
-
-        val flyway: Flyway = Flyway.configure()
-            .dataSource(VaadinOnKotlin.dataSource)
+         val flyway = Flyway.configure().cleanDisabled(false)
+            .dataSource(JdbiOrm.getDataSource())
             .load()
-//        flyway.migrate()
-        flyway.repair()
+        flyway.clean()
+        sleep(5000)
+
+
+//        val flyway: Flyway = Flyway.configure()
+//            .dataSource(VaadinOnKotlin.dataSource)
+//            .load()
+        flyway.migrate()
+//        flyway.repair()
 //         setup security
         // security interceptor is configured in AppServiceInitListener
 //        User(username = "admin", roles = "").apply { setPassword("admin"); save() }
